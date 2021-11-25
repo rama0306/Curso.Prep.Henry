f822ebc61d8b75e36111f7d53ad40f84377fc06e
ramiro godoy palacio <rama.nacionesph@gamil.com>
2021-11-25 14:59:30 -0600

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -73,10 +73,9 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1 === str2) {
-    return true;
-  } else {
-    return false;
+  return str1.length === str2.length;
+  
+
     
   }
 }
