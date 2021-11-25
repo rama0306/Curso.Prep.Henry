4c73a4862f75880492711096dc9d76bfa2ead36d
ramiro godoy palacio <rama.nacionesph@gamil.com>
2021-11-25 14:47:19 -0600

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -73,7 +73,12 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (str1.longuitud === str2.longuitud) {
+    return true;
+  } else {
+    return false;
+    
+  }
 }
 
 function menosQueNoventa(num) {
